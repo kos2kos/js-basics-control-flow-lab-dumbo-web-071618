59c6d3ea76b2a852b00ca1a2d3d3cdf37583ea81
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-08-26 22:09:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400){
+    return 'This one is on me!';
+  }
+   else if (someValue >= 2500){
+     return 'No can do.' ;
+   }
   
 }
 
+
+
+gives customers a free sample if the ride is less than or equal to 400 feet ‣
+AssertionError: expected undefined to equal 'This one is on me!'
+    at Context.<anonymous> (test/indexTest.js:6:45)
+charges 30 dollars for a distance over 2000 feet ‣
+AssertionError: expected undefined to equal 'I will gladly take your thirty bucks.'
+    at Context.<anonymous> (test/indexTest.js:10:46)
+does not allow rides over 2500 feet ‣
+AssertionError: expected undefined to equal 'No can do.'
+    at Context.<anonymous> (test/indexTest.js:14:46)
+    
+    
 function ternaryCheckCity(arg){}
 
 function switchOnCharmFromTip(arg){}
